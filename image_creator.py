41704bb00c9f23b9a1182260fde75775bf552313
eyvmal <eyvind.malde@gmail.com>
2024-04-17 22:40:50 +0200

    add logic when no secondary logo is found

--- image_creator.py
+++ image_creator.py
@@ -22,15 +22,14 @@ class ImageCreator:
             print(f"Error loading image from {path}: {e}")
             return None
 
-    def find_second_logo(self, first_line: str, image_map: dict) -> tuple:
+    def find_second_logo(self, first_line: str, image_map: dict):
         """Determine the second logo and title based on the first line of text."""
         for keywords, (image_name, title) in image_map.items():
             if any(keyword in first_line.lower() for keyword in keywords):
                 return image_name, title
         return None, None
 
-    def adjust_font_size(self, draw: ImageDraw.Draw, text: str, font: ImageFont.FreeTypeFont,
-                         image_width: int) -> ImageFont.FreeTypeFont:
+    def adjust_font_size(self, draw, text: str, font, image_width: int):
         """Adjust font size to ensure the text fits within the specified width."""
         text_width, _ = draw.textsize(text, font=font)
         while text_width > image_width and font.size > 10:
@@ -38,29 +37,32 @@ class ImageCreator:
             text_width, _ = draw.textsize(text, font=font)
         return font
 
-    def create_image(self, text: str, image_map: dict, league: str) -> Image:
+    def create_image(self, text: str, image_map: dict, league: str):
         background = self.load_image(self.background_path)
         brann_logo = self.load_image(self.brann_logo_path)
         if not background or not brann_logo:
+            print("Couldn't load Brann logo or background.")
             return None
 
         first_line = text.split('\n')[0]
         second_logo_name, title = self.find_second_logo(first_line, image_map)
+        opponent_found = True
         if not second_logo_name:
-            print("No matching keyword found in the first line.")
-            return None
+            print("Couldn't find opposing team in image_map.")
+            opponent_found = False
 
-        lines = text.split('\n')
-        lines[0] = f"{title}, {league}"
-        text = '\n'.join(lines)
+        if opponent_found:
+            second_logo_path = os.path.join(os.path.dirname(__file__), "images", second_logo_name)
+            second_logo = self.load_image(second_logo_path)
 
-        second_logo_path = os.path.join(os.path.dirname(__file__), "images", second_logo_name)
-        second_logo = self.load_image(second_logo_path)
-        if not second_logo:
-            return None
+            background.paste(brann_logo, (self.LOGO_BRANN_X, self.LOGO_PADDING_TOP), brann_logo)
+            background.paste(second_logo, (self.LOGO_SECOND_X, self.LOGO_PADDING_TOP), second_logo)
 
-        background.paste(brann_logo, (self.LOGO_BRANN_X, self.LOGO_PADDING_TOP), brann_logo)
-        background.paste(second_logo, (self.LOGO_SECOND_X, self.LOGO_PADDING_TOP), second_logo)
+            lines = text.split('\n')
+            lines[0] = f"{title}, {league}"
+            text = '\n'.join(lines)
+        else:
+            background.paste(brann_logo, (int((background.width - brann_logo.width) / 2), self.LOGO_PADDING_TOP), brann_logo)
 
         draw = ImageDraw.Draw(background)
         font = ImageFont.truetype(self.font_path, 100)
@@ -71,7 +73,7 @@ class ImageCreator:
 
         return background
 
-    def save_image(self, image: Image.Image, path: str) -> str | None:
+    def save_image(self, image, path: str):
         try:
             savepath = os.path.join(os.path.dirname(__file__), path)
             image.save(savepath)
