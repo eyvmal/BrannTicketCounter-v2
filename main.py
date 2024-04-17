c2e034d75c4b2de2a244cc2a69070ec8af4e386d
eyvmal <eyvind.malde@gmail.com>
2024-04-17 22:41:23 +0200

    add start message and TODOs

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 from clubs.brann.brann import run_brann
+from scrape_tools import get_time_formatted
+
+
+def start_script():
+    time_now = get_time_formatted("human")
+    print("========================================")
+    print(f"=  Running scripts,  {time_now}  =")
+    print("========================================")
+
 
 if __name__ == "__main__":
-    run_brann("all", False, False)
+    '''
+    TODO:
+    * Fikse opp i typer, return-verdi og beskrivelser av alle funksjonene.
+    * Error-håndtering
+        '''
+    start_script()
+    run_brann("next", True, False)
